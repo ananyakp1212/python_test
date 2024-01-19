0d0e23ade89aca134de1def338be7f905e40e35f
ananyakp1212 <149044071+ananyakp1212@users.noreply.github.com>
2024-01-19 08:40:13 +0530

    Create pttython_eg.py

---
+++ pttython_eg.py
@@ -0,0 +1,33 @@
+    # Open the input file in csv format and read data
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #get comments from specific column
+            comments = row.get(comments_column, '').strip()
+            #match the patterns of the comment
+            matches = re.findall(pattern, comments)
+            #update the count
+            groupCount.update(matches)
+
+    return groupCount
+
+def outputFile(output_file_path, groupCount):
+    #Results to be stored in output file
+    with open(output_file_path, 'w', encoding='utf-8') as outfile:
+        #Mention the headers
+        outfile.write("Group name\tNumber of occurrences\n")
+
+        # Write group names and occurrences
+        for group, count in groupCount.items():
+            outfile.write(f"{group}\t{count}\n")
+
+    print("Output is been written onto the file:", output_file_path)
+
+# Provide sample inputs as follws
+input_file_path = 'input.csv'
+output_file_path = 'outputGP.txt'
+comments_column_name = 'Additional comments'
+search_string = 'Groups'
+
+groups_counter = extract_count(input_file_path, comments_column_name, search_string)
+outputFile(output_file_path, groups_counter)
